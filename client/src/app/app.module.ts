8dc514ac31688ea51d25a842dbf6fb98fb5717d4
Maholy <maholy.ramaromanana@gmail.com>
2023-05-19 23:02:50 +0300

    navbar nearly done

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavigationComponent
+    NavigationComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
